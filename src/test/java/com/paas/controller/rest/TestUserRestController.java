0f7ef9943b70fe07dc775799943d930d5ef1c75f
Oleg Oseev <ooseev@yahoo.com>
2018-11-07 12:39:56 -0800

    added user not found test cases

--- src/test/java/com/paas/controller/rest/TestUserRestController.java
+++ src/test/java/com/paas/controller/rest/TestUserRestController.java
@@ -46,6 +46,16 @@ public class TestUserRestController {
 					.andExpect(content().contentType("application/json;charset=UTF-8"))
 					.andExpect(content().json("{'status': 'OK','success':'OK','data':{'name':'games','uid':5,'gid':60,'comment':'games','home':'/usr/games'}}"));
 	}
+
+	@Test
+	public void getUserNotFound() throws Exception {
+		// trying to get user - games:x:5:60:games:/usr/games:/usr/sbin/nologin
+		this.mockMvc.perform(get("/users/11"))
+					.andDo(print())
+					.andExpect(status().is4xxClientError())
+					.andExpect(content().contentType("application/json;charset=UTF-8"))
+					.andExpect(content().json("{'status': 'error','error':'NOT_FOUND'}"));
+	}
 	
 	@Test
 	public void getAllGroupsForUser() throws Exception {
@@ -59,6 +69,19 @@ public class TestUserRestController {
 					.andExpect(content().json("{'status': 'OK','success':'OK', 'data':[{'name':'sys','gid':3},{'name':'adm','gid':4}]}"));
 	}
 	
+	
+	@Test
+	public void getAllGroupsForUserNotFound() throws Exception {
+		// requesting groups for user - sys:x:3:3:sys:/dev:/usr/sbin/nologin
+		// the user should appear in two groups
+		this.mockMvc.perform(get("/users/11/groups"))
+					.andDo(print())
+					.andExpect(status().is4xxClientError())
+					.andExpect(content().contentType("application/json;charset=UTF-8"))
+					// check some key : value fields in the response
+					.andExpect(content().json("{'status': 'error','error':'NOT_FOUND'}"));
+	}
+	
 	@Test
 	public void queryUser() throws Exception {
 		// query user - mail:x:8:8:mail:/var/mail:/usr/sbin/nologin
@@ -66,6 +89,17 @@ public class TestUserRestController {
 			        .andDo(print())
 					.andExpect(status().isOk())
 					.andExpect(content().contentType("application/json;charset=UTF-8"))
-					.andExpect(content().json("{'status': 'OK','success':'OK','data':[{'name':'mail','uid':8,'gid':8,'comment':'mail','home':'/var/mail','shell':'/usr/sbin/nologin'}]}"));
+					.andExpect(content().json("{'status': 'OK','success':'OK',"
+							+ "'data':[{'name':'mail','uid':8,'gid':8,'comment':'mail','home':'/var/mail','shell':'/usr/sbin/nologin'}]}"));
+	}
+	
+	@Test
+	public void queryUserNotFound() throws Exception {
+		// query user - mail:x:8:8:mail:/var/mail:/usr/sbin/nologin
+		this.mockMvc.perform(get("/users/query?uid=11&name=unknown"))
+			        .andDo(print())
+					.andExpect(status().is4xxClientError())
+					.andExpect(content().contentType("application/json;charset=UTF-8"))
+					.andExpect(content().json("{'status': 'error','error':'NOT_FOUND'}"));
 	}
 }
