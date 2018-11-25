20abe3034db9d77b355976ba824bf08cb89fce84
Oleg Oseev <ooseev@yahoo.com>
2018-11-25 06:35:57 -0800

    add constructor with class template to exception handler

--- src/main/java/com/paas/PaaSApplicationException.java
+++ src/main/java/com/paas/PaaSApplicationException.java
@@ -1,27 +1,29 @@
 package com.paas;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PaaSApplicationException extends RuntimeException {
 
 
 	private static final long serialVersionUID = 4650037236983286671L;
 
 	private String errorMessage;
-	
-	public String getErrorMessage() {
-		return errorMessage;
-	}
-
-	public void setErrorMessage(String errorMessage) {
-		this.errorMessage = errorMessage;
-	}
 
 	public PaaSApplicationException() {
 		
 	}
 
 	public PaaSApplicationException(String message) {
-//		super(message);
+		super(message);
 		errorMessage = message;
+	}
+	
+	public <T> PaaSApplicationException(Class<T> aClass, String message) {
+		super(message);
+		Logger LOG = LoggerFactory.getLogger(aClass);
+		errorMessage = message;
+		LOG.error(message);
 	}
 	
 	public PaaSApplicationException(Throwable cause) {
@@ -35,5 +37,13 @@ public class PaaSApplicationException extends RuntimeException {
 	public PaaSApplicationException(String message, Throwable cause, boolean enableSuppression,
 			boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
 	}
 }
